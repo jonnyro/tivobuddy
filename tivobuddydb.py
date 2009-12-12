fffbdd0eb2f969bc64c9059cecc743e8b3ca821f
Jonathan Romero <jonnyro@rumega.cfl.rr.com>
2009-12-12 02:25:58 -0500

    Possibly using this for a database frontend for showcache and encode queue

---
+++ tivobuddydb.py
@@ -0,0 +1,20 @@
+class tivoBuddyDB:
+	def __init__():
+		pass
+
+	def initializeBackingStore():
+		pass
+	def addShowToCache(title="",description="",url=""):
+		pass
+	def clearShowCache():
+		pass
+	def getCacheContents():
+		pass
+	def markShowForEncode():
+		pass
+	def clearEncodeQueue():
+		pass
+
+if __name__ == "__main__":
+	print "TODO: Implement basic test of tivo db"
+
