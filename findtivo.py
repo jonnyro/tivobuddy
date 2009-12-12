88a321a815d4834991fceef8199f97344f3010f4
Jonathan Romero <jonnyro@rumega.cfl.rr.com>
2009-12-12 01:25:28 -0500

    Set up findtivo for standalone operation.  Move to object oriented layout.  Implement auto timeout for search

--- findtivo.py
+++ findtivo.py
@@ -3,6 +3,8 @@ from dbus import DBusException
 from dbus.mainloop.glib import DBusGMainLoop
 import sys
 import inspect, operator
+import signal, os
+
 #Look for tivo sharesi
 TYPE = '_tivo-videos._tcp'
 #dbus.String(u'Gaston')
@@ -10,72 +12,81 @@ TIVOS=frozenset(('TIVO1','TIVO2NAME','TIVO3NAME'))
 FOUND_TIVOS=set()
 TIVO_DATA = dict()
 
-def stop_scan():
-    global sbrowser
-    global TIVO_DATA
-    global main_loop
-#    for (key,val) in TIVO_DATA:
-#	print key + "," + val
-    #print TIVO_DATA
-    #sys.exit(0)
-    main_loop.quit()
-
-def service_resolved(*args):
-    global TIVOS
-    global FOUND_TIVOS
-    global TIVO_DATA
-
-    global finished
-    if args[2] in TIVOS:
-	FOUND_TIVOS.add(str(args[2]))
-    	TIVO_DATA[str(args[2])] = str(args[7])
-	if FOUND_TIVOS >= TIVOS:
-		#print "Stopping scan"
-		stop_scan()		
-    #print 'service resolved'
-    #print 'name:', args[2]
-    #print 'address:', args[7]
-    #print 'port:', args[8]
-
-def print_error(*args):
-    print 'error_handler'
-    print args[0]
-    
-def myhandler(interface, protocol, name, stype, domain, flags):
-    #print "Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
-
-    if flags & avahi.LOOKUP_RESULT_LOCAL:
-            # local service, skip
-            pass
-
-    server.ResolveService(interface, protocol, name, stype, 
-        domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
-        reply_handler=service_resolved, error_handler=print_error)
-
-
-def run_scan():
-	global server
-	global TIVO_DATA
-	global sbrowser
-	global main_loop
-
-	loop = DBusGMainLoop()
-
-	bus = dbus.SystemBus(mainloop=loop)
-
-	server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
-        'org.freedesktop.Avahi.Server')
-
-	sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,
-        	server.ServiceBrowserNew(avahi.IF_UNSPEC,
-            	avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
-       	 	avahi.DBUS_INTERFACE_SERVICE_BROWSER)
-
-	sbrowser.connect_to_signal("ItemNew", myhandler)
-
-	main_loop= gobject.MainLoop()
+class TivoHunter:
 	
-	main_loop.run()
 
-	return TIVO_DATA
-#print run_scan()
+	def __init__(self,maxDuration=5):
+		self.main_loop= gobject.MainLoop()
+		self.loop = DBusGMainLoop()
+		self.bus = dbus.SystemBus(mainloop=self.loop)
+
+		#Set up an alarm to stop scanning when maxDuration expires
+		signal.signal(signal.SIGALRM, self.stop_scan)
+		signal.alarm(maxDuration)
+		
+
+	def stop_scan(self,signum,frame):
+	    for (key,val) in TIVO_DATA:
+		print key + "," + val
+	    #print TIVO_DATA
+	    #sys.exit(0)
+	    self.main_loop.quit()
+
+	def service_resolved(self,*args):
+	    global TIVOS
+	    global FOUND_TIVOS
+	    global TIVO_DATA
+
+	    global finished
+	    if args[2] in TIVOS:
+		FOUND_TIVOS.add(str(args[2]))
+		TIVO_DATA[str(args[2])] = str(args[7])
+		if FOUND_TIVOS >= TIVOS:
+			#print "Stopping scan"
+			stop_scan()		
+	    print 'service resolved'
+	    print 'name:', args[2]
+	    print 'address:', args[7]
+	    print 'port:', args[8]
+
+	def print_error(self,*args):
+	    print 'error_handler'
+	    print args[0]
+	    
+	def myhandler(self,interface, protocol, name, stype, domain, flags):
+
+	    if flags & avahi.LOOKUP_RESULT_LOCAL:
+		    # local service, skip
+		    pass
+
+	    server.ResolveService(interface, protocol, name, stype, 
+		domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
+		reply_handler=self.service_resolved, error_handler=self.print_error)
+
+
+	def run_scan(self):
+		global server
+		global TIVO_DATA
+		global sbrowser
+		global main_loop
+
+
+		server = dbus.Interface( self.bus.get_object(avahi.DBUS_NAME, '/'),
+		'org.freedesktop.Avahi.Server')
+
+		sbrowser = dbus.Interface(self.bus.get_object(avahi.DBUS_NAME,
+			server.ServiceBrowserNew(avahi.IF_UNSPEC,
+			avahi.PROTO_UNSPEC, TYPE, 'local', dbus.UInt32(0))),
+			avahi.DBUS_INTERFACE_SERVICE_BROWSER)
+
+		sbrowser.connect_to_signal("ItemNew",self.myhandler)
+
+		
+		self.main_loop.run()
+
+		return TIVO_DATA
+
+if __name__ == "__main__":
+	a = TivoHunter()
+	a.run_scan()
+	signal.alarm(0)
