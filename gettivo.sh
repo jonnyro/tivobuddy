ebcadb1c76cc5efad8ec2d5305777b3a85290127
Jonathan Romero <jonnyro@rumega.cfl.rr.com>
2009-12-13 11:18:36 -0500

    gettivo.sh now accepts the MAK as the second argument.  IP of tivo is still the first.
    tivobuddy.py will now prompt for MAK, and will display shows from each tivo.  It now uses the findtivo module to locate tivos via UPnP

--- gettivo.sh
+++ gettivo.sh
@@ -1,1 +1,1 @@
-wget --no-check-certificate --http-user=tivo --http-password=YOURMAKHERE -O tivoroll.xml "https://$1/TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes"
+wget --no-check-certificate --http-user=tivo --http-password=$2 -O tivoroll.xml "https://$1/TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes"
--- tivobuddy.py
+++ tivobuddy.py
@@ -1,130 +1,96 @@
-TIVO="TIVO_IP"
 MAK="YOURMAKHERE"
 import ConfigParser
 import libxml2
 import libxslt
 import sys
 import urllib2
-import MySQLdb
 import os
-import findtivo
+from findtivo import TivoHunter
 
 MAK=-1
+class TivoBuddy:
 
-def checkTablesExist():
+	def __init__(self,MAK):
+		self.mak = MAK
 
-	pass
-
-def loadGlobalSettings():
-	global MAK
-	global DB_FILE
-
-	if (os.path.exists("config.ini")):
-		config = ConfigParser.ConfigParser()
-		config.readfp(open('config.ini'))
-	else:
-		print "Configuration File Not Found"
-
-	#Attempt to load media access key
-	try:
-		MAK=config.get("Account Settings","MediaAccessKey")
-
-	except:
-		print "Media Access Key not found in configuration file"
-		print ""
-		print "Format:"
-		print ""
-		print "[Account Settings]"
-		print "MediaAccessKey=111111132433"
-		print ""
-		print "Quitting"
-		sys.exit(-1)
-
-	#Attempt to load database filename
-	try:
-		DB_FILE = config.get("Database Settings","DBFileName")
-	except:
-		print "Database filename notfound in configuration file"
-		print ""
-		print "Format:"
-		print ""
-		print "[Database Settings]"
-		print "DBFileName=tivobuddy.sqlite"
-		print ""
-		print "Quitting"
-		sys.exit(-1)
 		
-def updateTivoShowCache(TIVO_ID, TIVO_IP):
-	conn = sqlite3.connect('tivo.db')
-	c = conn.cursor()
+	def updateTivoShowCache(self):
+		hunter = TivoHunter()
+		tivos = hunter.run_scan()
+		print tivos
+		for (key,value) in tivos.items():
+			print "Key: " + str(key)
+			print "Value: " + str(value)
+			ip = str(value)
+			name = str(key)	
+			print "Attempting to open tivo " + name + "  at: " + ip
+			url = "https://" + ip + "/TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes" 
+			print "URL=" + url
 
+			#TODO: Set up gettivo.sh to accept Media Access Key as argument.
+			cmd = "./gettivo.sh " + ip + " " + self.mak 
+			print "CMD is " + cmd
+			os.system(cmd)
 
+			styledoc = libxml2.parseFile("tivo.xsl")
+			style = libxslt.parseStylesheetDoc(styledoc)
+			doc = libxml2.parseFile("tivoroll.xml")
+			result = style.applyStylesheet(doc, None)
+			style.saveResultToFilename("tivoroll.html", result, 0)
+			style.freeStylesheet()
+			doc.freeDoc()
+			result.freeDoc()
 
-	print "Attempting to open tivo at: " + str(TIVO_IP) + " with ID " + str(TIVO_ID)
-	url = "https://" + TIVO_IP + "/TiVoConnect?Command=QueryContainer&Container=%2FNowPlaying&Recurse=Yes" 
-	print "URL=" + url
+			f=open('./tivoroll.html')
 
-	#TODO: Set up gettivo.sh to accept Media Access Key as argument.
-	cmd = "./gettivo.sh " + TIVO_IP
-	print "CMD is " + cmd
-	os.system(cmd)
+			linecount=0
+			for line in f:
+				linecount = linecount+1
 
-	styledoc = libxml2.parseFile("tivo.xsl")
-	style = libxslt.parseStylesheetDoc(styledoc)
-	doc = libxml2.parseFile("tivoroll.xml")
-	result = style.applyStylesheet(doc, None)
-	style.saveResultToFilename("tivoroll.html", result, 0)
-	style.freeStylesheet()
-	doc.freeDoc()
-	result.freeDoc()
+			f.close()
 
-	f=open('./tivoroll.html')
+			import math
 
-	linecount=0
-	for line in f:
-		linecount = linecount+1
+			f=open('./tivoroll.html')
 
-	f.close()
+			#Delete all shows for this tivo from the showbuffer
+			
+			   
+			#Iterate line by line over tivoroll.html converting each line
+			# into a format suitable for insertion into database
+			item = 0
+			curline = 0
+			for line in f:
+				linearr = line.split("|")
 
-	import math
-
-	f=open('./tivoroll.html')
-
-	#Delete all shows for this tivo from the showbuffer
-	query = "DELETE FROM showbuffer WHERE tivoID=\"%s\"" % str(TIVO_ID)			
-	cursor.execute( query )
-	
-	   
-	#Iterate line by line over tivoroll.html converting each line
-	# into a format suitable for insertion into database
-	item = 0
-	curline = 0
-	for line in f:
-		linearr = line.split("|")
-
-		if (linearr[0] == "Show"):
-			show = linearr[1]
-			episode_title = ""
-			description = ""
-			URL = ""
-		if (linearr[0] == "EpisodeTitle"):
-			episode_title = linearr[1]
-		if (linearr[0] == "Description"):
-			description = linearr[1]
-		if (linearr[0] == "URL"):
-			URL = linearr[1]
-			#insert show entry into database
-			query = "INSERT INTO showbuffer SET tivoID=\"%s\", showname=\"%s\",showtitle=\"%s\",showdescription=\"%s\",showurl=\"%s\"" % tuple(map(MySQLdb.escape_string,(str(TIVO_ID), show, episode_title, description, URL)))
-			cursor.execute( query )
-		curline = curline + 1
+				if (linearr[0] == "Show"):
+					show = linearr[1]
+					episode_title = ""
+					description = ""
+					URL = ""
+				if (linearr[0] == "EpisodeTitle"):
+					episode_title = linearr[1]
+				if (linearr[0] == "Description"):
+					description = linearr[1]
+				if (linearr[0] == "URL"):
+					URL = linearr[1]
+					#insert show entry into database
+		#			query = "INSERT INTO showbuffer SET tivoID=\"%s\", showname=\"%s\",showtitle=\"%s\",showdescription=\"%s\",showurl=\"%s\"" % tuple(map(MySQLdb.escape_string,(str(TIVO_ID), show, episode_title, description, URL)))
+					#cursor.execute( query )
+					#Now we have everything to add to db
+					print "Show: " + show
+					print "EP:   " + episode_title
+				curline = curline + 1
 
 
 
 if __name__ == "__main__":
-	#Confirm sqlite tables exist and match our specifications
-	if not checkTablesExist():
-		echo "Error initializing/connecting to sqlite database"
-		sys.exit(-1)
+	print "Please enter MAK"
+	mak = raw_input()
+	a = TivoBuddy(mak)
+	a.updateTivoShowCache()
+	print "Doing nothing"
+comment = """
 
 
 	conn = sqlite3.connect(
@@ -150,3 +116,45 @@ if __name__ == "__main__":
 		print "Tivo IP: " + ip
 		print "matching ID: " + str(ID)
 		updateTivoShowCache(ID,ip)
+
+
+	def loadGlobalSettings(self):
+		global MAK
+		global DB_FILE
+
+		if (os.path.exists("config.ini")):
+			config = ConfigParser.ConfigParser()
+			config.readfp(open('config.ini'))
+		else:
+			print "Configuration File Not Found"
+
+		#Attempt to load media access key
+		try:
+			MAK=config.get("Account Settings","MediaAccessKey")
+
+		except:
+			print "Media Access Key not found in configuration file"
+			print ""
+			print "Format:"
+			print ""
+			print "[Account Settings]"
+			print "MediaAccessKey=111111132433"
+			print ""
+			print "Quitting"
+			sys.exit(-1)
+
+		#Attempt to load database filename
+		try:
+			DB_FILE = config.get("Database Settings","DBFileName")
+		except:
+			print "Database filename notfound in configuration file"
+			print ""
+			print "Format:"
+			print ""
+			print "[Database Settings]"
+			print "DBFileName=tivobuddy.sqlite"
+			print ""
+			print "Quitting"
+			sys.exit(-1)
+
+"""
