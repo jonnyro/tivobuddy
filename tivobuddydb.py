800f94616929015e458738320d40eedb6562bdb1
Jonathan Romero <jonnyro@rumega.cfl.rr.com>
2009-12-13 04:26:59 -0500

    Improve basic implementation of database object to store show cache and encode queue.

--- tivobuddydb.py
+++ tivobuddydb.py
@@ -1,20 +1,43 @@
-class tivoBuddyDB:
-	def __init__():
-		pass
+#!/usr/bin/env/python
 
-	def initializeBackingStore():
+#TivoBuddyDB.py
+
+#Copyright (c) 2009 Jonathan S. Romero
+
+import uuid
+
+class Show:
+	def __init__(self,title,description=None,url=None):
+		self.title	 = title
+		self.description = description
+		self.url	 = url
+	def getTitle(self):
+		return self.title
+	def getDescription(self):
+		return self.description
+	def getURL(self):
+		return self.url
+
+class TivoBuddyDB:
+	def __init__(self):
+		self.shows = []
+		self.encodequeue = []
+	def initializeBackingStore(self):
 		pass
-	def addShowToCache(title="",description="",url=""):
-		pass
-	def clearShowCache():
-		pass
-	def getCacheContents():
-		pass
-	def markShowForEncode():
-		pass
-	def clearEncodeQueue():
-		pass
+	def addShowToCache(self,ShowObj):
+		GUID = uuid.uuid4()
+		self.shows.append((GUID,ShowObj))
+	def clearShowCache(self):
+		self.shows = []
+	def getCacheContents(self):
+		return self.shows	
+	def markShowForEncode(self,GUID):
+		self.encodequeue.append(GUID)
+	def clearEncodeQueue(self):
+		self.encodequeue=[]
 
 if __name__ == "__main__":
-	print "TODO: Implement basic test of tivo db"
-
+	print "Basic Tests of TivoBuddyDB"
+	db = TivoBuddyDB()
+	db.addShowToCache(Show(title="Knight Rider",description="Car jumps over another car"))
+	print db.getCacheContents()
